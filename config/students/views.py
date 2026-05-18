bc5ebb6a68d332904fe26dff0c477b880d970a7e
Musthak-PT <musthakpt786@gmail.com>
2026-05-18 13:21:33 +0530

    Initial project setups

---
+++ config/students/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from .models import Student
+from .serializers import StudentSerializer
+
+# GET all students + POST
+class StudentListCreateView(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+# GET one + PUT + PATCH + DELETE
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
