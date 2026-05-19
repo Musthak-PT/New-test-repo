4839b2519944eca2be7f6dce5d3f2529d3284de7
Musthak-PT <musthakpt786@gmail.com>
2026-05-19 11:54:26 +0530

    changes in the project

--- config/students/urls.py
+++ config/students/urls.py
@@ -3,5 +3,6 @@ from .views import StudentListCreateView, StudentDetailView
 
 urlpatterns = [
     path('students/', StudentListCreateView.as_view(), name='student-list-create'),
+    
     path('students/<int:pk>/', StudentDetailView.as_view(), name='student-detail'),
 ]
